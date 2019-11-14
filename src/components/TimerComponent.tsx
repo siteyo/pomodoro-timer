cb878f0fb08f73649c96640d7ab412f5a7cda8cd
siteyo <phicyanoc@gmail.com>
2019-11-14 23:04:39 +0900

    Add textfields to specify the time

--- src/components/TimerComponent.tsx
+++ src/components/TimerComponent.tsx
@@ -5,6 +5,7 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
 
 import { makeStyles } from '@material-ui/styles';
 
@@ -15,6 +16,12 @@ const useStyles = makeStyles({
   time: {
     fontSize: 40,
     textAlign: 'center',
+  },
+  textField: {
+    marginLeft: 1,
+    marginRight: 1,
+    width: 200,
+    display: 'block',
   },
 });
 
@@ -35,6 +42,25 @@ const TimerComponent: FC<TimerProps> = ({ timeLeft, start, stop, reset }) => {
           <Typography className={classes.time}>
             {Math.floor(timeLeft / 60)}:{('00' + (timeLeft % 60)).slice(-2)}
           </Typography>
+
+          <TextField
+            className={classes.textField}
+            label="Work [min]"
+            type="number"
+            defaultValue={25}
+          />
+          <TextField
+            className={classes.textField}
+            label="Interval [min]"
+            type="number"
+            defaultValue={5}
+          />
+          <TextField
+            className={classes.textField}
+            label="Repeat"
+            type="number"
+            defaultValue={4}
+          />
         </CardContent>
         <CardActions>
           <Button onClick={start}>Start</Button>
